ea884c53518e4803451b323f22136b13fcdc738f
ibrahimsow <ibrahim.s@codeur.online>
2019-02-07 15:49:43 +0000

    config menu

--- wp-content/themes/inpulsa/functions.php
+++ wp-content/themes/inpulsa/functions.php
@@ -4,12 +4,14 @@
 
 define('INPULSA_VERSION', '1.0.1');
 function inpulsa_scripts(){
+
     //chargement des styles
     wp_enqueue_style( 'inpulsa_bootstrap-core', get_template_directory_uri() . '/css/bootstrap.min.css', array(), INPULSA_VERSION, 'all');
-
     wp_enqueue_style( 'inpulsa_custom', get_template_directory_uri() . '/style.css', array('inpulsa_bootstrap-core'), INPULSA_VERSION, 'all');
+
     //chargement des js
-    wp_enqueue_script( 'inpulsa_admin_script', get_template_directory_uri() . '/js/inpulsa.js', array('jquery'), INPULSA_VERSION, true );
+    wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), INPULSA_VERSION, true );
+    wp_enqueue_script( 'inpulsa_admin_script', get_template_directory_uri() . '/js/inpulsa.js', array('jquery', 'bootstrap-js'), INPULSA_VERSION, true );
 
 }
 
@@ -32,6 +34,20 @@ function inpulsa_setup() {
 // support des vignettes
 add_theme_support('post-thumbnails');
 
+//enlève le générateur de version
+remove_action('wp_head', 'wp_generator');
+
+//enlève les guillemets à la française
+remove_filter ('the_content', 'wptexturize');
+
+// support du titre 
+add_theme_support('title-tag');
+
+// register custom Navigation Walker
+require_once('includes/wb_bootstrap_navwalker.php');
+
+//activer la gestion des menus
+register_nav_menus( array('primary' => 'principal', 'secondary' => 'deuxième' ) );
 }
 
 add_action('after_setup_theme', 'inpulsa_setup');
--- wp-content/themes/inpulsa/index.php
+++ wp-content/themes/inpulsa/index.php
@@ -1,14 +1,100 @@
-<html lang="en">
+<html <?php language_attributes(); ?>>
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo('charset');?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Ce site est le blog de la promo 23 inpulsa Team L'ACS de Vesoul">
     <?php wp_head(); ?>
-    <title>Inpulsa</title>
+
+
 </head>
 <body>
-    <h1>blog inpulsa team </h1>
-    <p class="container">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque fugiat itaque praesentium cupiditate mollitia porro iusto excepturi, numquam obcaecati at eius, accusamus optio quo, incidunt commodi consequatur. Quam, nemo explicabo!</p>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="/">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <!--<div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Disabled</a>
+                    </li>
+                </ul>
+            </div>-->
+
+
+            <?php
+            wp_nav_menu( array(
+                'theme_location'  => 'primary',
+                'depth'	          => 2, // 1 = no dropdowns, 2 = with dropdowns.
+                'container'       => 'div',
+                'container_class' => 'collapse navbar-collapse',
+                'container_id'    => 'bs-example-navbar-collapse-1',
+                'menu_class'      => 'navbar-nav mr-auto',
+                'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+                'walker'          => new WP_Bootstrap_Navwalker(),
+            ) );
+            ?>
+
+
+
+        </nav>
+    </header>
+    <div class="container">
+        <div class="jumbotron">
+            <h1>Hello bienvenue sur sur le blog</h1>
+        </div>
+    </div>
+
+      
+    
+
+    <section>
+        <div class="container">
+
+    <?php if (have_posts()): ?>
+
+    <?php  while(have_posts()): the_post(); ?>
+
+
+        <div class="row">
+            <div class="mb-5 col-2">
+               <?php the_post_thumbnail('thumbnail'); ?>
+            </div>
+            <div class="mb-5 col-10">
+                <h1> <?php the_title(); ?> </h1>
+                <?php the_excerpt(); ?>
+            </div>
+        </div>
+        <?php  endwhile; ?>
+        
+        <?php   else: ?>
+            <div class="row">
+                <div class="col-12">
+                    <p>
+                    il n'y a pas de résultats
+                    </p>
+                </div>
+            </div>
+    
+        <?php  
+                
+                endif;
+                        
+            ?>
+        </div>
+    </section>
+  
     <?php wp_footer(); ?>
 </body>
 </html>
