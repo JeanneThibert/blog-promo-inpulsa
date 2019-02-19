39c72bad33b4362b4f827f8cc3732f732b253268
aureliendeloye <aurelien.d@codeur.online>
2019-02-19 10:11:01 +0000

    debut de creation de la paje des articles

--- wp-content/themes/inpulsa/home.php
+++ wp-content/themes/inpulsa/home.php
@@ -1,35 +1,63 @@
+<?php
+/*
+Template Name: articles
+*/
 
-<?php get_header(); ?>
+
+
+
+
+
+ get_header(); ?>
+ 
+ <div class="img_header_article container-fluid m-0 p-0">
+     <div class="texte">Articles de veille<br>
+      technologique</div>
+</div>
+
+<form class="search-container">
+    <input type="text" id="search-bar" placeholder="Recherchez un article ?">
+    <a href="#"><img class="search-icon" src="http://www.endlessicons.com/wp-content/uploads/2012/12/search-icon.png"></a>
+  </form>
 
 <section>
+       
     <div class="container">
-
-<?php if (have_posts()): ?>
-
-<?php  while(have_posts()): the_post(); ?>
-
-<?php echo $post->post_title; ?>
-
-<?php get_template_part('content'); ?>
-
-    <?php  endwhile; ?>
+        
     
-    <?php   else: ?>
-        <div class="row">
-            <div class="col-12">
-                <p>
-                il n'y a pas de résultats
-                </p>
-            </div>
-        </div>
 
-    <?php  
+        <?php if (have_posts()): ?>
+
+        <?php  while(have_posts()): the_post(); ?>
+
+        <?php echo $post->post_title; ?>
+
+        <?php get_template_part('content'); ?>
+
+        <?php  endwhile; ?>
+    
+        <?php   else: ?>
+            <div class="row">
+                <div class="col-12">
+            
+                    <p>
+                    il n'y a pas de résultats
+                    </p>
+                </div>
+            </div>
+
+       
+
+            <?php  
             
             endif;
                     
-        ?>
+            ?>
+            
     </div>
 </section>
 
 <?php get_footer(); ?>
+
+
 
