8730d3b9f915a3be4c490870ee661a44e2d621d2
JeanneThib <jeanne.t@codeur.online>
2019-02-20 07:24:07 +0000

    [WORDPRESS] modification de la page des membres et galeries , ajout des différentes section

--- wp-content/themes/inpulsa/equipe.php
+++ wp-content/themes/inpulsa/equipe.php
@@ -6,47 +6,54 @@ Template Name: Équipe
 ?>
 <?php get_header(); ?>
 
-<section class="container">
+<section class="container-fluid section-equipe d-flex justify-content-center align-items-center">
+
+    
+</section>
+
+<section class="container-fluid">
+<div class="container"> 
+<h1 class="team-membre">Les Membres</h1>
    
     <div class="row">
-        <div class="col">
+        <div class="col-12 col-sm-12 col-md-12 col-lg-2">
             <img class="img-fluid image--cover" src="../../../gravit/photo/abdelkrim.jpg" alt="abdelkrim">
             <p class="d-flex justify-content-center">Abdelkrim Naji</p>
         </div>
-        <div class="col">
+        <div class="col-12 col-sm-12 col-md-12 col-lg-2">
             <img class="img-fluid image--cover" src="../../../gravit/photo/antonin.jpg" alt="antonin">
             <p class="d-flex justify-content-center">Antonin Landry</p>
         </div>
-        <div class="col">
+        <div class="col-12 col-sm-12 col-md-12 col-lg-2">
             <img class="img-fluid image--cover" src="../../../gravit/photo/aurelien.jpg" alt="aurelien">
             <p class="d-flex justify-content-center">Aurélien Deloye</p>
         </div>
-        <div class="col">
+        <div class="col-12 col-sm-12 col-md-12 col-lg-2">
             <img class="img-fluid image--cover" src="../../../gravit/photo/elodie.jpg" alt="elodie">
             <p class="d-flex justify-content-center">Élodie Martin</p>
         </div>
-        <div class="col">
+        <div class="col-12 col-sm-12 col-md-12 col-lg-2">
             <img class="img-fluid image--cover" src="../../../gravit/photo/fabien.jpg" alt="fabien">
             <p class="d-flex justify-content-center">Fabien Cazelet </p>
         </div>
     </div>
 
     <div class="row">
-        <div class="col">
+        <div class="col-12 col-sm-12 col-md-12 col-lg-2">
             <img class="img-fluid image--cover" src="../../../gravit/photo/gaetan.jpg" alt="gaetan">
             <p class="d-flex justify-content-center">Gaetan Emond</p>
         </div>
-        <div class="col">
+        <div class="col-12 col-sm-12 col-md-12 col-lg-2">
             <p class=" text-inpulsa d-flex justify-content-center">Inpulsa</p>
         </div>
-        <div class="col">
+        <div class="col-12 col-sm-12 col-md-12 col-lg-2">
             <img class="img-fluid image--cover" src="../../../gravit/photo/manu.jpg" alt="manu">
             <p class="d-flex justify-content-center">Emmanuel Bailly-Salin</p>
         </div>
-        <div class="col">
+        <div class="col-12 col-sm-12 col-md-12 col-lg-2">
             <p class=" text-inpulsa d-flex justify-content-center">Team</p>
         </div>
-        <div class="col">
+        <div class="col-12 col-sm-12 col-md-12 col-lg-2">
             <img class="img-fluid image--cover" src="../../../gravit/photo/geoffroy.jpg" alt="geoffroy">
             <p class="d-flex justify-content-center">Geoffroy Carette </p>
         </div>
@@ -55,28 +62,93 @@ Template Name: Équipe
 
 
 <div class="row">
-        <div class="col">
+        <div class="col-12 col-sm-12 col-md-12 col-lg-2">
             <img class="img-fluid image--cover" src="../../../gravit/photo/ibrahim.jpg" alt="ibrahim">
             <p class="d-flex justify-content-center">Ibrahim Sow</p>
         </div>
-        <div class="col">
+        <div class="col-12 col-sm-12 col-md-12 col-lg-2">
             <img class="img-fluid image--cover" src="../../../gravit/photo/jeanne.jpg" alt="jeanne">
             <p class="d-flex justify-content-center">Jeanne Thibert </p>
         </div>
-        <div class="col">
+        <div class="col-12 col-sm-12 col-md-12 col-lg-2">
             <img class="img-fluid image--cover" src="../../../gravit/photo/jules.jpg" alt="jules">
             <p class="d-flex justify-content-center">Jules Pageaux </p>
         </div>
-        <div class="col">
+        <div class="col-12 col-sm-12 col-md-12 col-lg-2">
             <img class="img-fluid image--cover" src="../../../gravit/photo/lucas.jpg" alt="lucas">
             <p class="d-flex justify-content-center">Lucas Robin </p>
         </div>
-        <div class="col">
+        <div class="col-12 col-sm-12 col-md-12 col-lg-2">
             <img class="img-fluid image--cover" src="../../../gravit/photo/valerie.jpg" alt="valerie">
             <p class="d-flex justify-content-center">Valérie Honoré </p>
         </div>
-
+        </div>
     </div>
+</section>
+<section class="container">
+<div class="toolbar mb2 mt2">
+  <button class=" btn button fil-cat" href="" data-rel="all">All</button>
+  <button class=" btn button fil-cat" data-rel="web">Websites</button>
+  <button class="btn button fil-cat" data-rel="flyers">Flyers</button>
+  <button class="btn button fil-cat" data-rel="bcards">Business Cards</button>
+</div> 
+ 
+<div style="clear:both;"></div>   
+<div id="portfolio">
+  <div class="tile scale-anm web all">
+        <img src="http://demo.themerain.com/charm/wp-content/uploads/2015/04/2-mon_1092-300x234.jpg" alt="" />
+  </div>
+  <div class="tile scale-anm bcards all">
+    <img src="http://demo.themerain.com/charm/wp-content/uploads/2015/04/jti-icons_08-300x172.jpg" alt="" />
+  </div>
+  <div class="tile scale-anm web all">
+    <img src="http://demo.themerain.com/charm/wp-content/uploads/2015/04/emi_haze-300x201.jpg" alt="" />
+  </div>
+  <div class="tile scale-anm web all">
+            <img src="http://demo.themerain.com/charm/wp-content/uploads/2015/04/codystretch-300x270.jpg" alt="" />
+  </div>
+  <div class="tile scale-anm flyers all">
+        <img src="https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97190&w=350&h=190" alt="" />
+  </div>
+  <div class="tile scale-anm bcards all">
+            <img src="https://placeholdit.imgix.net/~text?txtsize=19&txt=200%C3%97290&w=200&h=290" alt="" />
+  </div>
+  <div class="tile scale-anm flyers all">
+    <img src="http://demo.themerain.com/charm/wp-content/uploads/2015/04/jti-icons_08-300x172.jpg" alt="" />
+  </div>
+  <div class="tile scale-anm flyers all">
+    <img src="http://demo.themerain.com/charm/wp-content/uploads/2015/04/transmission_01-300x300.jpg" alt="" />
+  </div>
+  <div class="tile scale-anm web all">
+        <img src="https://placeholdit.imgix.net/~text?txtsize=19&txt=200%C3%97290&w=200&h=290" alt="" />
+  </div>
+  <div class="tile scale-anm flyers all">
+           <img src="https://placeholdit.imgix.net/~text?txtsize=19&txt=200%C3%97290&w=200&h=290" alt="" /> 
+  </div>
+  <div class="tile scale-anm web all">
+        <img src="http://demo.themerain.com/charm/wp-content/uploads/2015/04/the-ninetys-brand_02-300x300.jpg" alt="" />
+  </div>
+  <div class="tile scale-anm bcards all">
+            <img src="http://demo.themerain.com/charm/wp-content/uploads/2015/04/15-dia_1092-1-300x300.jpg" alt="" />
+  </div>
+  <div class="tile scale-anm web all">
+       <img src="https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97190&w=350&h=190" alt="" /> 
+  </div>
+  <div class="tile scale-anm bcards all">
+          <img src="http://demo.themerain.com/charm/wp-content/uploads/2015/04/emi_haze-300x201.jpg" alt="" />  
+  </div>
+  <div class="tile scale-anm web all">
+            <img src="http://demo.themerain.com/charm/wp-content/uploads/2015/04/transmission_01-300x300.jpg" alt="" />
+  </div> 
+  <div class="tile scale-anm web all">
+      <img src="https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97190&w=350&h=190" alt="" />  
+  </div> 
+  <div class="tile scale-anm bcards all">     
+            <img src="https://placeholdit.imgix.net/~text?txtsize=19&txt=200%C3%97290&w=200&h=290" alt="" />
+  </div>
+</div>
+
+<div style="clear:both;"></div>   
 </section>
 
 
--- wp-content/themes/inpulsa/js/inpulsa.js
+++ wp-content/themes/inpulsa/js/inpulsa.js
@@ -60,3 +60,18 @@ window.onload = function() {
     css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
     document.body.appendChild(css);
 };
+
+// Funtiion pour les images
+$(function() {
+    var selectedClass = "";
+    $(".fil-cat").click(function(){ 
+    selectedClass = $(this).attr("data-rel"); 
+ $("#portfolio").fadeTo(100, 0.1);
+    $("#portfolio div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
+setTimeout(function() {
+  $("."+selectedClass).fadeIn().addClass('scale-anm');
+  $("#portfolio").fadeTo(300, 1);
+}, 300); 
+    
+});
+});
