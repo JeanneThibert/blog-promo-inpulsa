34810a6b659dbfff83aca6a0a402105ebceee9d7
ibrahimsow <ibrahim.s@codeur.online>
2019-02-27 13:45:06 +0000

    page accueil responsive

--- wp-content/themes/inpulsa/front-page.php
+++ wp-content/themes/inpulsa/front-page.php
@@ -13,7 +13,7 @@ $req_blog = new WP_Query($args_blog); ?>
 
 <section class="container-fluid section-accueil d-flex justify-content-center align-items-center">
     <h1>
-      <a href="" class="typewrite" data-period="2000" data-type='[ "Une ambiance qui motive", "Une méthode qui fait ses preuves", "Une filière pleine davenir", "Une reconversion qui me réussit" ]'>
+      <a href="" class="typewrite" data-period="2000" data-type='[ "Une ambiance qui motive", "Une méthode qui fait ses preuves", "Une filière pleine d\u0027avenir", "Une reconversion qui me réussit" ]'>
         <span class="wrap"></span>
       </a>
     </h1>
@@ -27,17 +27,17 @@ $req_blog = new WP_Query($args_blog); ?>
     </div>
     <div class="text-presentation">
         <div class="row">
-            <div class="col-12 col-sm-12 col-md-12 col-lg-4 d-flex flex-column justify-content-start align-items-center">
+            <div class="mb-5 col-12 col-sm-12 col-md-12 col-lg-4 d-flex flex-column justify-content-start align-items-center">
                 <img src="../../../gravit/agile.png" alt="" class="mb-4">
                 <h3 class="text-center">Méthode</h3>
                 <p class="">À travers des projets nous apprenons à utiliser les languages et outils de dévelopement web. La méthode agile nous donne la possibilité de travailler selon nos préférences ce qui armonise le travail en équipe et permet de retenir plus facilement.</p>
             </div>
-            <div class="col-12 col-sm-12 col-md-12 col-lg-4 d-flex flex-column justify-content-start align-items-center">
+            <div class="mb-5 col-12 col-sm-12 col-md-12 col-lg-4 d-flex flex-column justify-content-start align-items-center">
                 <img src="../../../gravit/polyvalence.png" alt="" class="mb-4">
                 <h3 class="text-center">Polyvalence</h3>
                 <p class="">Pour arriver à un projet fini, il est souvent nécessaire d'avoir des connaissances en front-end et back end. Le front est plus axé sur se que l'utilisateur voit, le back lui concerne la partie fonctionement du projet. Nous avons la chance d'aborder les deux cotés se qui nous permet de devenir autonome.</p>
             </div>
-            <div class="col-12 col-sm-12 col-md-12 col-lg-4 d-flex flex-column justify-content-start align-items-center">
+            <div class="mb-5 col-12 col-sm-12 col-md-12 col-lg-4 d-flex flex-column justify-content-start align-items-center">
                 <img src="../../../gravit/realisme.png" alt="" class="mb-4">
                 <h3 class="text-center">Réalisme</h3>
                 <p class="">Parce que le monde du travail est plein d'exigences et demande du professionnalisme, nous passons 2 mois en entreprise afin de pratiqué ce que nous voyons pendant la formation. Cela nous permet de nous projeté vers un futur emploi dans le web.</p>
@@ -51,7 +51,7 @@ $req_blog = new WP_Query($args_blog); ?>
 <section id="blog-front" class="section-projet d-flex flex-column justify-content-center align-items-center">
     <h4 class="text-center mb-5">Nos projets récents</h4>
     <img src="../../../gravit/lg_logo_langues.png" alt="techno" class="techno img-fluid mb-5">
-    <div class="container-fluid all-projets">
+    <div class="container-fluid all-projets pt-5 pb-5">
         <div class="container">
             <div class="row">
                 
